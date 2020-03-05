8fc7ba862aff80f15c416c3ab595944406b09662
Lee Hoyong <igotoweb@gmail.com>
2020-03-06 04:00:18 +0900

    fix a typo remove FILL_ME_IN variables

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,15 +1,13 @@
 import { connect } from "react-redux";
 import Nav from "../components/Nav";
 
-const FILL_ME_IN = "FILL_ME_IN";
-
 const mapStateToProps = state => ({
   // TODO:
   // presentational 컴포넌트 Nav가 스토어로부터 user 및 darkMode 속성을 받고 싶어합니다.
   // mapStateToProps는 스토어의 상태(State)를 presentational 컴포넌트의 속성으로(ToProps) mapping 합니다.
   // App 컴포넌트 아래에 힌트가 있습니다.
 
-  받아야할_속성: FILL_ME_IN
+  받아야할_속성: undefined
 });
 
 export default connect(mapStateToProps)(Nav);
