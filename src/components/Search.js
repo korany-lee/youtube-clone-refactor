8955adb6aa7da7fd18bf2ef669d10fa0ea99c210
Koeun Lee <korany@kakao.com>
2020-11-15 17:16:14 +0900

    youtube clone coding from codestates also refactor some funtions

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,43 +1,49 @@
 import React from "react";
 
 class Search extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      value: ""
-    };
-  }
+		this.state = {
+			value: "",
+		};
+	}
 
-  handleInputChange(e) {
-    // this.props.handleSearchInputChange(e.target.value);
-    this.setState({
-      value: e.target.value
-    });
-  }
+	handleInputChange(e) {
+		// this.props.handleSearchInputChange(e.target.value);
+		this.setState({
+			value: e.target.value,
+		});
+	}
 
-  handleSearch() {
-    this.props.handleSearchInputChange(this.state.value);
-  }
+	handleSearch() {
+		this.props.handleSearchInputChange(this.state.value);
+	}
 
-  render() {
-    return (
-      <div className="search-bar form-inline">
-        <input
-          className="form-control"
-          type="text"
-          value={this.state.value}
-          onChange={this.handleInputChange.bind(this)}
-        />
-        <button
-          className={this.props.darkMode ? "btn btn-dark" : "btn btn-light"}
-          onClick={this.handleSearch.bind(this)}
-        >
-          <span className="glyphicon glyphicon-search"></span>
-        </button>
-      </div>
-    );
-  }
+	onKeyPress(event) {
+		if (event.key === "Enter") {
+			this.handleSearch();
+		}
+	}
+
+	render() {
+		return (
+			<div className="search-bar form-inline">
+				<input
+					className="form-control"
+					type="text"
+					value={this.state.value}
+					onChange={this.handleInputChange.bind(this)}
+					onKeyPress={this.onKeyPress.bind(this)}
+				/>
+				<button
+					className={this.props.darkMode ? "btn btn-dark" : "btn btn-light"}
+					onClick={this.handleSearch.bind(this)}>
+					<span className="glyphicon glyphicon-search"></span>
+				</button>
+			</div>
+		);
+	}
 }
 
 export default Search;
--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,66 +1,71 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-class Setting extends React.Component {
-  constructor(props) {
-    super(props);
+//Class => hooks 바꾸기 연습
+const Setting = ({
+	isOpen,
+	handleClose,
+	handleUpdateSetting,
+	user,
+	darkMode,
+}) => {
+	//useRef를 사용하지 않으면 어떤 에러가 발생하나:
+	const nameRef = useRef();
 
-    this.state = {
-      username: props.user ? props.user.name : ""
-    };
-  }
+	const [username, setUsername] = useState({
+		name: user.name ? user.name : "이름없음",
+	});
 
-  handleChangeName(event) {
-    this.setState({
-      username: event.target.value
-    });
-  }
+	const handleChangeName = event => {
+		event.preventDefault();
+		const value = nameRef.current.value;
+		setUsername({ name: value });
+	};
 
-  handleSave() {
-    this.props.handleUpdateSetting("currentUser", {
-      name: this.state.username
-    });
-  }
+	const handleSave = () => {
+		handleUpdateSetting("currentUser", {
+			name: username.name,
+		});
+		handleClose(); //저장한 뒤에도 창이 닫히게
+	};
 
-  handleToggleDarkMode(event) {
-    this.props.handleUpdateSetting("darkMode", event.target.checked);
-  }
+	const handleToggleDarkMode = event => {
+		handleUpdateSetting("darkMode", event.target.checked);
+	};
 
-  render() {
-    return (
-      <div className={this.props.isOpen ? "mdl show" : "mdl"}>
-        <div className="mdl-mask" onClick={this.props.handleClose}></div>
-        <div className="sidebar">
-          <h3>Setting</h3>
-          <hr />
+	return (
+		<div className={isOpen ? "mdl show" : "mdl"}>
+			<div className="mdl-mask" onClick={handleClose}></div>
+			<div className={darkMode ? "sidebar dark" : "sidebar"}>
+				<h3>Setting</h3>
+				<hr />
 
-          <fieldset>
-            <legend>사용자 설정</legend>
-            <span className="small-margin-right">사용자 이름</span>
-            <input
-              type="text"
-              className="small-margin-right"
-              value={this.state.username}
-              onChange={this.handleChangeName.bind(this)}
-            />
-            <button onClick={this.handleSave.bind(this)}>저장</button>
-          </fieldset>
+				<fieldset>
+					<legend>사용자 설정</legend>
+					<span className="small-margin-right">사용자 이름</span>
+					<input
+						ref={nameRef}
+						type="text"
+						className="small-margin-right"
+						value={username.name}
+						onChange={handleChangeName}
+					/>
+					<button onClick={handleSave}>저장</button>
+				</fieldset>
 
-          <hr />
-          <fieldset>
-            <legend>일반</legend>
-            <label>
-              <input
-                type="checkbox"
-                className="small-margin-right"
-                onChange={this.handleToggleDarkMode.bind(this)}
-              ></input>
-              <span>Dark Mode</span>
-            </label>
-          </fieldset>
-        </div>
-      </div>
-    );
-  }
-}
+				<hr />
+				<fieldset>
+					<legend>일반</legend>
+					<label>
+						<input
+							type="checkbox"
+							className="small-margin-right"
+							onChange={handleToggleDarkMode}></input>
+						<span>Dark Mode</span>
+					</label>
+				</fieldset>
+			</div>
+		</div>
+	);
+};
 
 export default Setting;
